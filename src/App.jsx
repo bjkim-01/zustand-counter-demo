07d4b414ecb4344bae7a1744153321e065085628
bjkim <bjkimgithub@gmail.com>
2025-11-02 09:14:26 +0900

    감소 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import CountBox from './components/CountBox'
-import counterStore from './stores/counterStore';
+import { useState } from "react";
+import "./App.css";
+import CountBox from "./components/CountBox";
+import counterStore from "./stores/counterStore";
 
 function App() {
   // const [count, setCount] = useState(0)
-  const { count, increase, increaseBy } = counterStore();
+  const { count, increase, increaseBy, decrease, decreaseBy } = counterStore();
 
   return (
     <div>
       <h1>count:{count}</h1>
-      <button onClick={increase}>increase</button>
-      <button onClick={() => increaseBy(10)}>10씩 증가 </button>
+
+      <div>
+        <button onClick={increase}>increase</button>
+        <button onClick={() => increaseBy(10)}>10씩 증가 </button>
+      </div>
+
+      <div>
+        <button onClick={decrease}>decrease</button>
+        <button onClick={() => decreaseBy(10)}>10씩 감소 </button>
+      </div>
+      
       <CountBox />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
